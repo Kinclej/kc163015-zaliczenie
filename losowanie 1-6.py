477203dc34db876b0d2a884688211b3514006223
Kinclej <kcelinia@gmail.com>
2024-06-30 14:16:10 +0200

    Program teraz losuje więcej liczb

--- losowanie 1-6.py
+++ losowanie 1-6.py
@@ -2,3 +2,10 @@ import random
 x = random.randrange(1, 7) #losuje liczby od 1 do 6
 y = 0
 print ("Wynik rzutu kostką to: ", x)
+print ("Wynik rzutu kostką to: ", x)
+y += x # jest to skrót działania y = y+x
+x = random.randrange(1, 7) #losuje liczby od 1 do 6
+print ("Wynik rzutu kostką to: ", x)
+y += x# jest to skrót działania y = y+x
+x = random.ranrange(1, 7) #losuje liczby od 1 do 6
+print ("Wynik rzutu kostką to: ", x)
--- tekst.py
+++ tekst.py
@@ -1,1 +1,2 @@
 print("Lubie kotki")
+print("Lubie jelenie")
