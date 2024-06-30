10960ecaedf73333c216073ac537ac521404b618
Kinclej <kcelinia@gmail.com>
2024-06-30 14:20:20 +0200

    Naprawione błędy w kodzie, dodana została możliwość wygranej

--- losowanie 1-6.py
+++ losowanie 1-6.py
@@ -1,11 +1,17 @@
 import random
 x = random.randrange(1, 7) #losuje liczby od 1 do 6
 y = 0
-print ("Wynik rzutu kostką to: ", x)
+print ("Wyrzuć wynik 18 aby wygrać: ")
 print ("Wynik rzutu kostką to: ", x)
 y += x # jest to skrót działania y = y+x
 x = random.randrange(1, 7) #losuje liczby od 1 do 6
 print ("Wynik rzutu kostką to: ", x)
 y += x# jest to skrót działania y = y+x
-x = random.ranrange(1, 7) #losuje liczby od 1 do 6
+x = random.randrange(1, 7) #losuje liczby od 1 do 6
 print ("Wynik rzutu kostką to: ", x)
+y += x# jest to skrót działania y = y+x
+   
+if y >= 18:
+    print ("Wygrywasz! Wynik to: ", y)
+else:
+    print ("Spróbuj jeszcze raz! Twój wynik to: ", y)
